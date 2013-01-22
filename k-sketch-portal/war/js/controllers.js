88645c7dffb6a4b8fb354c1f3fa49ce0631686b0
Shannon <panguini2010@gmail.com>
2013-01-22 13:21:27 +0800

    Shannon: Intermediary code for Google Login implementation (HELP!!!!).

--- k-sketch-portal/war/js/controllers.js
+++ k-sketch-portal/war/js/controllers.js
@@ -1,9 +1,15 @@
 'use strict';
 
+<% page import="com.google.appengine.api.users.*"; %>
+
 /* Controllers */
 
 
 function FirstController($scope) {
-   $scope.name = "Prof. Richard";
+   <%
+	 UserService userService = UserServiceFactory.getUserService();
+     String username = (String) session.getAttribute("username");
+    %>
+   $scope.name = <% username; %>;
 }
 
