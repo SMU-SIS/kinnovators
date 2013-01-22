2a855d32703b884fc53050bde104469f4e2adad1
Brandon Goh <kwgoh.2010@sis.smu.edu.sg>
2013-01-22 13:41:35 +0800

    Brandon: Able to transfer XML String to Angular.JS controller. Next step will be to create key-mapping for saved strings (ala Java HashMap).

--- k-sketch-portal/war/KSketchCallBacks.js
+++ k-sketch-portal/war/KSketchCallBacks.js
@@ -1,58 +1,64 @@
-// JavaScript Document
+'use strict';
+	
+	//THIS FILE IS NOW USELESS, USELESS, USELESS!
+	//MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA........
 
-//Function receives an xml string from the flash object
-//And displays it in the text box
-function saveKSketchFile(fileData)
-{
-	var textAreaObject = document.getElementById('visibleTextData');
-	if(typeof textAreaObject !='undefined'){
-		textAreaObject.value = fileData
-	}
-	else
+	//Function to trigger a save procedure in the flash object
+	//The flash object will call saveKSketchFile in return to pass the data from itself to javascript
+	function initSave()
 	{
-		alert('no text area!');
-	}	
-}
-
-//Function receives an xml string from anywhere
-//and asks the flash object to load it
-function loadKSketchFile(fileData)
-{
-	var swfObject = document.getElementById('KSketch2_Web');
-	if(typeof swfObject !='undefined'){
-		swfObject.load(fileData);//change load
+		var swfObject = document.getElementById('KSketch2_Web');
+		if(typeof swfObject !='undefined'){
+			swfObject.save();//change load
+			
+		}
+		else
+		{
+			alert('the swf object does not exist! Unable to save');
+		}	
 	}
-	else
+	
+	//Don't care about this, it just allows this example to load a file from
+	//The text area
+	function initLoad()
 	{
-		alert('the swf object does not exist! Unable to save');
-	}	
-}
-
-//Function to trigger a save procedure in the flash object
-//The flash object will call saveKSketchFile in return to pass the data from itself to javascript
-function initSave()
-{
-	var swfObject = document.getElementById('KSketch2_Web');
-	if(typeof swfObject !='undefined'){
-		swfObject.save();//change load
+		var textAreaObject = document.getElementById('visibleTextData');
+		if(typeof textAreaObject !='undefined'){
+			loadKSketchFile(textAreaObject.value);
+			//loadKSketchFile(textAreaObject.innerText);
+		}
+		else
+		{
+			alert('no text area!');
+		}	
 	}
-	else
-	{
-		alert('the swf object does not exist! Unable to save');
-	}	
-}
 
-//Don't care about this, it just allows this example to load a file from
-//The text area
-function initLoad()
-{
-	var textAreaObject = document.getElementById('visibleTextData');
-	if(typeof textAreaObject !='undefined'){
-		loadKSketchFile(textAreaObject.value);
+	// JavaScript Document
+	
+	//Function receives an xml string from the flash object
+	//And displays it in the text box
+	function saveKSketchFile(fileData)
+	{
+		//var textAreaObject = document.getElementById('visibleTextData');
+		//textAreaObject.value = fileData;	
+		
+		e = document.getElementById('SaveSketch');
+		scope = angular.element(e).scope();
+		scope.$apply(function() {
+			scope.setData(fileData);
+		});	
 	}
-	else
+	
+	//Function receives an xml string from anywhere
+	//and asks the flash object to load it
+	function loadKSketchFile(fileData)
 	{
-		alert('no text area!');
-	}	
-}
-
+		var swfObject = document.getElementById('KSketch2_Web');
+		if(typeof swfObject !='undefined'){
+			swfObject.load(fileData);//change load
+		}
+		else
+		{
+			alert('the swf object does not exist! Unable to save');
+		}	
+	}
--- k-sketch-portal/war/js/controllers.js
+++ k-sketch-portal/war/js/controllers.js
@@ -2,8 +2,25 @@
 
 /* Controllers */
 
-
+//angular.module("myApp", []);
 function FirstController($scope) {
-   $scope.name = "Prof. Richard";
-}
+    $scope.name = "Prof. Richard";
+	
+  	$scope.fileData = "?";  
+   	$scope.fileName = "";
+	$scope.files = [];
+	$scope.filenames = [];
+	   
+	$scope.save = function() {
+	   	
+		
+		$scope.filenames.push($scope.fileName);
+		$scope.files.push($scope.fileData);
+	   		
+	}
+   
+	$scope.setData = function(f) {
+		$scope.fileData = f;
+	}
 
+}
