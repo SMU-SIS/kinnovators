df68de170f49d6fcb8d439d2989914a1142f910c
Brandon Goh <kwgoh.2010@sis.smu.edu.sg>
2013-02-25 15:13:56 +0800

    Brandon: current-app version as of 25022013 back to gh-pages.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,28 @@
 
 /* Controllers */
 
+//angular.module("myApp", []);
+function FirstController($scope,$resource) {
+    $scope.name = "Prof. Richard";
+    
+    //resource
+    $scope.backend_locations = "";
+    
+	//local
+  	$scope.fileData = "?";  
+   	$scope.fileName = "";
 
-function FirstController($scope) {
-   $scope.name = "Prof. Richard";
-}
+	$scope.filearray = [];
 
-function AnnimationController($scope, $resource) {
-   $scope.name = $resource('/animation').get();
+	$scope.save = function() {
+
+		$scope.fileData = $scope.fileData.replace(/(\r\n|\n|\r)/gm," ");
+		$scope.filearray.push({id: $scope.filearray.length + 1, name: $scope.fileName, data: $scope.fileData})
+	   	$scope.fileData = "";
+
+	}
+   
+	$scope.setData = function(f) {
+		$scope.fileData = f;
+	}
 }
--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@
 
 Created on Dec 6, 2012
 @author: Chris Boesch
+@modified: Goh Kian Wei (Brandon)
 """
 """
 Note to self: json.loads = json string to objects. json.dumps is object to json string.
@@ -17,7 +18,175 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 import json
 
-class Backend(db.Model):
+"""
+Attributes and functions for Sketch entity
+"""
+class Sketch(db.Model):
+  apikey = db.StringProperty(required=True,default='Default-APIKey')
+  model = db.StringProperty(required=True,default='Default-Model')
+  #Use backend record id as the model id for simplicity
+  jsonString = db.TextProperty(required=True,default='{}')
+  created = db.DateTimeProperty(auto_now_add=True) #The time that the model was created    
+  modified = db.DateTimeProperty(auto_now=True)
+  
+  def to_dict(self):
+       d = dict([(p, unicode(getattr(self, p))) for p in self.properties()])
+       d["id"] = self.key().id()
+       return d
+
+      
+  @staticmethod
+  def add(apikey, model, data):
+    #update ModelCount when adding
+    jsonData = json.loads(data)
+    modelCount = ModelCount.all().filter('en_type','Sketch').filter('apikey',apikey).filter('model', model).get()
+    if modelCount:
+      modelCount.count += 1
+      modelCount.put()
+    else:
+      modelCount = ModelCount(en_type='Sketch', apikey=apikey, model=model, count=1)
+      modelCount.put()
+    
+	#For sketch files saved through "Save As"
+    if jsonData['sketchId'] == '':
+      jsonData['sketchId'] = str(modelCount.count)
+	#For sketch files saved through "Save As" that were not derived from another file  
+    if jsonData['original'] == ':':
+      jsonData['original'] = 'original'
+     
+    entity = Sketch(apikey=apikey,
+                    model=model,
+                    jsonString=json.dumps(jsonData))
+    
+    entity.put()
+    
+    result = {'model':model,
+              'apikey': apikey,
+              'id': entity.key().id(), 
+              'data': jsonData} #this would also check if the json submitted was valid
+        
+    return result
+  
+  @staticmethod
+  def get_entities(apikey, model=None, offset=0, limit=50):
+    #update ModelCount when adding
+    theQuery = Sketch.all().filter('apikey',apikey)
+    if model:
+      theQuery = theQuery.filter('model', model)
+
+    objects = theQuery.fetch(limit=limit, offset=offset)
+
+    entities = []
+    for object in objects:
+      entity = {'model':object.model,
+              'apikey': apikey,
+              'id': object.key().id(),
+              'created': object.created,
+              'modified': object.modified, 
+              'data': json.loads(object.jsonString)}
+      entities.append(entity)
+    
+    count = 0
+    modelCount = ModelCount.all().filter('en_type','Sketch').filter('apikey',apikey).filter('model', model).get()
+    if modelCount:
+      count = modelCount.count
+    result = {'method':'get_entities',
+              'apikey': apikey,
+              'en_type': 'Sketch',
+              'model': model,
+              'count': count,
+              'offset': offset,
+              'limit':limit,
+              'entities': entities}      
+    return result
+    
+  @staticmethod
+  def get_entity(apikey,model,model_id):
+    theobject = Sketch.get_by_id(int(model_id))
+    
+    result = {'method':'get_model',
+                  'apikey': apikey,
+                  'model': model,
+                  'id': model_id,
+                  'data': json.loads(theobject.jsonString)
+                  }
+    return result
+  
+  @staticmethod
+  def clear(apikey, model):
+    #update model count when clearing model on api
+    count = 0
+    for object in Sketch.all().filter('apikey',apikey).filter('model', model):
+      count += 1
+      object.delete()
+      
+    modelCount = ModelCount.all().filter('en_type','Sketch').filter('apikey',apikey).filter('model', model).get()
+    if modelCount:
+      modelCount.delete()
+    result = {'items_deleted': count}
+    return result
+    
+  @staticmethod
+  def clearapikey(apikey):
+    #update model count when clearing model on api
+    count = 0
+    for object in Sketch.all().filter('apikey',apikey):
+      count += 1
+      object.delete()
+      
+    modelCount = ModelCount.all().filter('en_type','Sketch').filter('apikey',apikey).get()
+    if modelCount:
+      modelCount.delete()
+    result = {'items_deleted': count}
+    return result
+  
+  #You can't name it delete since db.Model already has a delete method
+  @staticmethod
+  def remove(apikey, model, model_id):
+  	#update model count when deleting
+  	entity = Sketch.get_by_id(int(model_id))
+  	
+  	if entity and entity.apikey == apikey and entity.model == model:
+  		entity.delete()
+  	
+  		result = {'method':'delete_model_success',
+                  'apikey': apikey,
+                  'model': model,
+                  'id': model_id
+                  }
+  	else:
+  		result = {'method':'delete_model_not_found'}
+  		
+  	modelCount = ModelCount.all().filter('en_type','Sketch').filter('apikey',apikey).filter('model', model).get()
+  	if modelCount:
+  		modelCount.count -= 1
+  		modelCount.put()
+  	
+  	return result
+
+  #data is a dictionary that must be merged with current json data and stored. 
+  @staticmethod
+  def edit_entity(apikey, model, model_id, data):
+    jsonString = data
+    entity = Sketch.get_by_id(int(model_id))
+    entity.jsonString = jsonString
+    entity.put()
+    if entity.jsonString:
+      data = json.loads(entity.jsonString)
+    else:
+      data = {}
+    result = {'model':model,
+              'apikey': apikey,
+              'id': entity.key().id(), 
+              'data': data #this would also check if the json submitted was valid
+              }
+    return result
+
+
+"""
+Attributes and functions for User entity
+"""
+class User(db.Model):
   apikey = db.StringProperty(required=True,default='Default-APIKey')
   model = db.StringProperty(required=True,default='Default-Model')
   #Use backend record id as the model id for simplicity
@@ -35,17 +204,17 @@ class Backend(db.Model):
   def add(apikey, model, data):
     #update ModelCount when adding
     jsonString = data
-    entity = Backend(apikey=apikey,
+    entity = User(apikey=apikey,
                     model=model,
                     jsonString=jsonString)
     
     entity.put()
-    modelCount = ModelCount.all().filter('apikey',apikey).filter('model', model).get()
+    modelCount = ModelCount.all().filter('en_type','User').filter('apikey',apikey).filter('model', model).get()
     if modelCount:
       modelCount.count += 1
       modelCount.put()
     else:
-      modelCount = ModelCount(apikey=apikey, model=model, count=1)
+      modelCount = ModelCount(en_type='User', apikey=apikey, model=model, count=1)
       modelCount.put()
     
     result = {'model':model,
@@ -58,7 +227,7 @@ class Backend(db.Model):
   @staticmethod
   def get_entities(apikey, model=None, offset=0, limit=50):
     #update ModelCount when adding
-    theQuery = Backend.all().filter('apikey',apikey)
+    theQuery = User.all().filter('apikey',apikey)
     if model:
       theQuery = theQuery.filter('model', model)
 
@@ -75,11 +244,12 @@ class Backend(db.Model):
       entities.append(entity)
     
     count = 0
-    modelCount = ModelCount.all().filter('apikey',apikey).filter('model', model).get()
+    modelCount = ModelCount.all().filter('en_type','User').filter('apikey',apikey).filter('model', model).get()
     if modelCount:
       count = modelCount.count
     result = {'method':'get_entities',
               'apikey': apikey,
+              'en_type': 'User',
               'model': model,
               'count': count,
               'offset': offset,
@@ -89,7 +259,7 @@ class Backend(db.Model):
     
   @staticmethod
   def get_entity(apikey,model,model_id):
-    theobject = Backend.get_by_id(int(model_id))
+    theobject = User.get_by_id(int(model_id))
     
     result = {'method':'get_model',
                   'apikey': apikey,
@@ -103,11 +273,11 @@ class Backend(db.Model):
   def clear(apikey, model):
     #update model count when clearing model on api
     count = 0
-    for object in Backend.all().filter('apikey',apikey).filter('model', model):
+    for object in User.all().filter('apikey',apikey).filter('model', model):
       count += 1
       object.delete()
       
-    modelCount = ModelCount.all().filter('apikey',apikey).filter('model', model).get()
+    modelCount = ModelCount.all().filter('en_type','User').filter('apikey',apikey).filter('model', model).get()
     if modelCount:
       modelCount.delete()
     result = {'items_deleted': count}
@@ -117,11 +287,11 @@ class Backend(db.Model):
   def clearapikey(apikey):
     #update model count when clearing model on api
     count = 0
-    for object in Backend.all().filter('apikey',apikey):
+    for object in User.all().filter('apikey',apikey):
       count += 1
       object.delete()
       
-    modelCount = ModelCount.all().filter('apikey',apikey).get()
+    modelCount = ModelCount.all().filter('en_type','User').filter('apikey',apikey).get()
     if modelCount:
       modelCount.delete()
     result = {'items_deleted': count}
@@ -131,7 +301,7 @@ class Backend(db.Model):
   @staticmethod
   def remove(apikey, model, model_id):
   	#update model count when deleting
-  	entity = Backend.get_by_id(int(model_id))
+  	entity = User.get_by_id(int(model_id))
   	
   	if entity and entity.apikey == apikey and entity.model == model:
   		entity.delete()
@@ -144,7 +314,7 @@ class Backend(db.Model):
   	else:
   		result = {'method':'delete_model_not_found'}
   		
-  	modelCount = ModelCount.all().filter('apikey',apikey).filter('model', model).get()
+  	modelCount = ModelCount.all().filter('en_type','User').filter('apikey',apikey).filter('model', model).get()
   	if modelCount:
   		modelCount.count -= 1
   		modelCount.put()
@@ -155,7 +325,7 @@ class Backend(db.Model):
   @staticmethod
   def edit_entity(apikey, model, model_id, data):
     jsonString = data
-    entity = Backend.get_by_id(int(model_id))
+    entity = User.get_by_id(int(model_id))
     entity.jsonString = jsonString
     entity.put()
     if entity.jsonString:
@@ -167,11 +337,12 @@ class Backend(db.Model):
               'id': entity.key().id(), 
               'data': data #this would also check if the json submitted was valid
               }
-    return result
-
+    return result	
+	
 #Quick retrieval for supported models metadata and count stats
 class ModelCount(db.Model):
   apikey = db.StringProperty(required=True,default='Default-APIKey')
+  en_type = db.StringProperty(required=True,default='Default-entype')
   model = db.StringProperty(required=True,default='Default-Model')
   count = db.IntegerProperty(required=True, default=0)
   created = db.DateTimeProperty(auto_now_add=True) #The time that the model was created    
@@ -227,7 +398,7 @@ class ActionHandler(webapp.RequestHandler):
         if new_offset:
             offset = int(new_offset)
 
-        result = Backend.get_entities(apikey,offset=offset)
+        result = Sketch.get_entities(apikey,offset=offset)
         
         filename = "Backup_"+apikey+"_offset_"+str(offset)+".json"
         self.response.headers['Content-Type'] = 'application/streaming-json'
@@ -251,7 +422,7 @@ class ActionHandler(webapp.RequestHandler):
         self.response.headers['Content-Type'] = 'application/streaming-json'
         self.response.content_disposition = 'attachment; filename="'+filename+'"'
         
-        for entity in Backend.all():
+        for entity in Sketch.all():
           self.response.out.write(json.dumps(entity.to_dict(),default=dthandler)+"\n")
         return
 
@@ -261,18 +432,18 @@ class ActionHandler(webapp.RequestHandler):
     def clear_apikey(self,apikey):
         """Clears the datastore for a an apikey. 
 				"""
-        result = Backend.clearapikey(apikey)
+        result = Sketch.clearapikey(apikey)
         return self.respond({'method':'clear_apikey'})
       
     def clear_model(self,apikey, model):
         """Clears the datastore for a model and apikey.
         """
-      	result = Backend.clear(apikey, model)
+      	result = Sketch.clear(apikey, model)
         return self.respond(result)
 
     def add_or_list_model(self,apikey,model):
       	#Check for GET paramenter == model to see if this is an add or list. 
-      	#Call Backend.add(apikey, model, data) or
+      	#Call Sketch.add(apikey, model, data) or
         #Fetch all models for apikey and return a list. 
               	
         #Todo - Check for method.
@@ -280,7 +451,7 @@ class ActionHandler(webapp.RequestHandler):
         if self.request.method=="POST":
           logging.info("in POST")
           logging.info(self.request.body)
-          result = Backend.add(apikey, model, self.request.body)
+          result = Sketch.add(apikey, model, self.request.body)
           #logging.info(result)
           return self.respond(result)
     
@@ -288,19 +459,19 @@ class ActionHandler(webapp.RequestHandler):
           data = self.request.get("obj")
           if data: 
             logging.info("Adding new data: "+data)
-            result = Backend.add(apikey, model, data)
+            result = Sketch.add(apikey, model, data)
           else:
             offset = 0
             new_offset = self.request.get("offset")
             if new_offset:
               offset = int(new_offset)
 
-            result = Backend.get_entities(apikey, model,offset=offset)
+            result = Sketch.get_entities(apikey, model,offset=offset)
           
       	  return self.respond(result)
 
     def delete_model(self,apikey,model, model_id):
-      	result = Backend.remove(apikey,model, model_id)
+      	result = Sketch.remove(apikey,model, model_id)
       	
       	return self.respond(result)
       
@@ -314,24 +485,24 @@ class ActionHandler(webapp.RequestHandler):
 
         if self.request.method=="DELETE":
           logging.info("It was options")
-          result = Backend.remove(apikey,model, model_id)
+          result = Sketch.remove(apikey,model, model_id)
           logging.info(result)
           return self.respond(result)#(result)
         
         elif self.request.method=="PUT":
           logging.info("It was PUT")
           logging.info(self.request.body)
-          result = Backend.edit_entity(apikey,model,model_id,self.request.body)
-          #result = Backend.remove(apikey,model, model_id)
+          result = Sketch.edit_entity(apikey,model,model_id,self.request.body)
+          #result = Sketch.remove(apikey,model, model_id)
           #result = json.loads(self.request.body)
           #logging.info(result)
           return self.respond(result)#(result)          
       	else:
           data = self.request.get("obj")
       	  if data:
-      		  result = Backend.edit_entity(apikey,model,model_id,data)
+      		  result = Sketch.edit_entity(apikey,model,model_id,data)
       	  else:
-      		  result = Backend.get_entity(apikey,model,model_id)
+      		  result = Sketch.get_entity(apikey,model,model_id)
       	  return self.respond(result)
 
 application = webapp.WSGIApplication([
